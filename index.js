4ad06bc06a3532c894cc1e7fefe5b7b9bdafe718
Shreekrishna <hegdeshreekrishna469@gmail.com>
2025-12-09 22:27:24 +0530

    msg

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+const onlineUsers = new Map(); 
+// key: socket_id   value: user info (id, name)
 
 const mongoURI = process.env.MONGO_URL;
 mongoose.connect(mongoURI)
@@ -23,19 +25,60 @@ app.use("/api/auth", authRoutes);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+let socketCounter = 0; // unique socket id generator
+
 wss.on("connection", (ws) => {
-  console.log("Client connected via WebSocket");
+  const socketId = `s_${socketCounter++}`;
 
-  ws.send(JSON.stringify({ msg: "Connected to TransCrypt Cloud" }));
+  console.log("Client connected:", socketId);
 
-  ws.on("message", (msg) => {
-    console.log("Received:", msg.toString());
+  // TEMP until login is integrated
+  onlineUsers.set(socketId, {
+    id: socketId,
+    name: "Unknown User",
   });
 
+  // Notify THIS client of the current list
+  ws.send(JSON.stringify({
+    type: "online_users",
+    users: Array.from(onlineUsers.values())
+  }));
+
+  // Notify EVERYONE that a user came online
+  broadcast({
+    type: "user_joined",
+    user: onlineUsers.get(socketId)
+  });
+
+  // When server receives message
+  ws.on("message", (msg) => {
+    console.log("WS Message:", msg.toString());
+  });
+
+  // On disconnect
   ws.on("close", () => {
-    console.log("Client disconnected");
+    const user = onlineUsers.get(socketId);
+    onlineUsers.delete(socketId);
+
+    console.log("Client disconnected:", socketId);
+
+    broadcast({
+      type: "user_left",
+      user
+    });
   });
 });
+
+function broadcast(data) {
+  const json = JSON.stringify(data);
+
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(json);
+    }
+  });
+}
+
 
 
 server.listen(port, () => console.log(`Server running on port ${port}`));
