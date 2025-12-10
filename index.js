9082a38ae5ccd481f9fbcb5457bcadd4b8f2f84e
Shreekrishna <hegdeshreekrishna469@gmail.com>
2025-12-10 09:47:17 +0530

    fixed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
 const mongoose = require("mongoose");
-const User = require("./models/User");
+const User = require("./models/auth");
 
 const authRoutes = require("./routes/auth");
 
